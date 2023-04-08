7e982eb7bc883e15a06788e23c5097d13d582343
Sohana-500apps <sohana.500apps@gmail.com>
2023-04-08 14:32:29 +0530

    comments added in interviee

--- pages/TestInterview.vue
+++ pages/TestInterview.vue
@@ -1,7 +1,10 @@
 <template>
+     <!-- start of Interview tag -->
     <CollectionInterviewMain :InterViewUrl="InterViewUrl" :url="url"/>
+     <!-- End of Interview tag -->
 </template>
 <script setup lang="ts">
+//importing the Interview url and url
 let InterViewUrl="https://v7-stark-db-orm.mercury.infinity-api.net/api/mock-interviews/?offset=0&limit=100&sort_column=id&sort_direction=desc"
 let url="https://v7-stark-db-orm.mercury.infinity-api.net/api/mock-interviews"
 </script>
